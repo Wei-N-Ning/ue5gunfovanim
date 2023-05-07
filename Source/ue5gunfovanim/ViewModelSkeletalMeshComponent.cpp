9ac25ed4aa62b4d6b5bd9add8454f7bbd6a12df0
Wei <macgnw@gmail.com>
2023-05-07 06:10:11 -0500

    wip

--- Source/ue5gunfovanim/ViewModelSkeletalMeshComponent.cpp
+++ Source/ue5gunfovanim/ViewModelSkeletalMeshComponent.cpp
@@ -89,7 +89,7 @@ FMatrix UViewModelSkeletalMeshComponent::GetRenderMatrix() const
 		};
 	}
 
-	const FTransform ComponentTransform = GetComponentTransform();
+	FTransform ComponentTransform = GetComponentTransform();
 	AddDebugMessage(FString::Printf(TEXT("%ls tf: %ls"), *GetName(), *ComponentTransform.ToString()));
 	const FMatrix NewViewProjectionMatrix = ViewMatrix * NewProjectionMatrix;
 	const FMatrix InverseOldViewProjectionMatrix = InverseViewProjectionMatrix;
